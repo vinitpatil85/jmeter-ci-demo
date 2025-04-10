184da07d1b9efe2753b931aa731ebc610fdb6e6a
vinitpatil85 <2022AC05311@wilp.bits-pilani.ac.in>
2025-04-11 02:10:35 +0530

    Use full path for ThreadGroup.jmx

--- run-jmeter.bat
+++ run-jmeter.bat
@@ -1,13 +1,12 @@
-echo Script is running from: %cd%
-dir
-
-
 @echo off
 
-REM Set JMeter bin path (no quotes inside variable)
+REM Set JMeter bin path
 set JMETER_PATH=C:\Users\VINIT\Downloads\apache-jmeter-5.6.3\apache-jmeter-5.6.3\bin
 
-REM Run the JMeter test (quote paths where needed)
-"%JMETER_PATH%\jmeter.bat" -n -t "ThreadGroup.jmx" -l results.jtl -e -o report
+REM Use full path to .jmx file
+set TEST_PATH=%cd%\ThreadGroup.jmx
+
+REM Run the JMeter test
+"%JMETER_PATH%\jmeter.bat" -n -t "%TEST_PATH%" -l results.jtl -e -o report
 
 echo JMeter test executed.
