de0715af88168cef4813558f8d6ac3bfe01ea2da
vinitpatil85 <2022AC05311@wilp.bits-pilani.ac.in>
2025-04-11 02:13:31 +0530

    Add debug for full path

--- run-jmeter.bat
+++ run-jmeter.bat
@@ -3,8 +3,11 @@
 REM Set JMeter bin path
 set JMETER_PATH=C:\Users\VINIT\Downloads\apache-jmeter-5.6.3\apache-jmeter-5.6.3\bin
 
-REM Use full path to .jmx file
+REM Get full path to JMX file
 set TEST_PATH=%cd%\ThreadGroup.jmx
+
+REM Echo path for debugging
+echo Running test from: %TEST_PATH%
 
 REM Run the JMeter test
 "%JMETER_PATH%\jmeter.bat" -n -t "%TEST_PATH%" -l results.jtl -e -o report
