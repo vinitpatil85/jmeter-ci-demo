0394faa8388764565c9f6747ba31c15126e8ae43
vinitpatil85 <2022AC05311@wilp.bits-pilani.ac.in>
2025-04-11 02:00:11 +0530

    Fixed jmx file reference1

--- run-jmeter.bat
+++ run-jmeter.bat
@@ -4,6 +4,6 @@ REM Set JMeter bin path (no quotes inside variable)
 set JMETER_PATH=C:\Users\VINIT\Downloads\apache-jmeter-5.6.3\apache-jmeter-5.6.3\bin
 
 REM Run the JMeter test (quote paths where needed)
-"%JMETER_PATH%\jmeter.bat" -n -t "Thread Group.jmx" -l results.jtl -e -o report
+"%JMETER_PATH%\jmeter.bat" -n -t "ThreadGroup.jmx" -l results.jtl -e -o report
 
 echo JMeter test executed.
