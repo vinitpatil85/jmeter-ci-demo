2daea13fc6c36863ef6b8532ac7d9981e8ae9041
vinitpatil85 <2022AC05311@wilp.bits-pilani.ac.in>
2025-04-11 11:51:11 +0530

    report folder cleanup jtl file

--- run-jmeter.bat
+++ run-jmeter.bat
@@ -4,11 +4,18 @@ REM Set JMeter path
 set JMETER_PATH=C:\apache-jmeter-5.6.3\bin
 set TEST_PATH=%cd%\ThreadGroup.jmx
 set REPORT_PATH=%cd%\report
+set RESULTS_FILE=%cd%\results.jtl
 
-REM Clean report folder if it exists
+REM Clean report directory
 if exist "%REPORT_PATH%" (
     echo Deleting existing report directory...
     rmdir /s /q "%REPORT_PATH%"
+)
+
+REM Clean results file
+if exist "%RESULTS_FILE%" (
+    echo Deleting existing results.jtl file...
+    del /f /q "%RESULTS_FILE%"
 )
 
 REM Run JMeter test
