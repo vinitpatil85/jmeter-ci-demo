9523e8e31c89afd61174b0dac53f53cac74726ef
vinitpatil85 <2022AC05311@wilp.bits-pilani.ac.in>
2025-04-11 10:28:08 +0530

    Added file existence

--- run-jmeter.bat
+++ run-jmeter.bat
@@ -1,14 +1,17 @@
 @echo off
 
-REM Set JMeter bin path to a SYSTEM-accessible location
+REM JMeter path
 set JMETER_PATH=C:\apache-jmeter-5.6.3\bin
-
-REM Get full path to JMX file in Jenkins workspace
 set TEST_PATH=%cd%\ThreadGroup.jmx
 
-echo Running test from: %TEST_PATH%
+REM Debug: confirm file exists
+echo Looking for JMX file at: %TEST_PATH%
 
-REM Run the JMeter test
+if not exist "%TEST_PATH%" (
+    echo ERROR: File not found!
+    dir
+    exit /b 1
+)
+
+REM Run test
 "%JMETER_PATH%\jmeter.bat" -n -t "%TEST_PATH%" -l results.jtl -e -o report
-
-echo JMeter test executed.
